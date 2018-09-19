0beb09e3367b5ad6bc25e7440203f4115506f460
Stuart Lang <stuart.b.lang@gmail.com>
2018-09-20 00:02:36 +0100

    Add missing generic types to signatures

--- src/GlobalExceptionHandler.ContentNegotiation.Mvc/MessageFormatter.cs
+++ src/GlobalExceptionHandler.ContentNegotiation.Mvc/MessageFormatter.cs
@@ -27,36 +27,36 @@ namespace GlobalExceptionHandler.ContentNegotiation.Mvc
             formatter.WithBody(Formatter);
         }
         
-        public static void WithBody<T, TException>(this IHandledFormatters<TException> formatter, Func<Exception, T> f) where TException: Exception
+        public static void WithBody<T, TException>(this IHandledFormatters<TException> formatter, Func<TException, T> f) where TException: Exception
         {
             Task Formatter(Exception e, HttpContext c, HandlerContext b)
             {
                 c.Response.ContentType = null;
-                c.WriteAsyncObject(f(e));
+                c.WriteAsyncObject(f((TException)e));
                 return Task.CompletedTask;
             }
 
             formatter.WithBody(Formatter);
         }
         
-        public static void UsingMessageFormatter<T, TException>(this IHandledFormatters<TException> formatter, Func<Exception, HttpContext, T> f) where TException : Exception
+        public static void UsingMessageFormatter<T, TException>(this IHandledFormatters<TException> formatter, Func<TException, HttpContext, T> f) where TException : Exception
         {
             Task Formatter(Exception e, HttpContext c, HandlerContext b)
             {
                 c.Response.ContentType = null;
-                c.WriteAsyncObject(f(e, c));
+                c.WriteAsyncObject(f((TException)e, c));
                 return Task.CompletedTask;
             }
 
             formatter.WithBody(Formatter);
         }
         
-        public static void UsingMessageFormatter<T, TException>(this IHandledFormatters<TException> formatter, Func<Exception, HttpContext, HandlerContext, T> f) where TException : Exception
+        public static void UsingMessageFormatter<T, TException>(this IHandledFormatters<TException> formatter, Func<TException, HttpContext, HandlerContext, T> f) where TException : Exception
         {
             Task Formatter(Exception e, HttpContext c, HandlerContext b)
             {
                 c.Response.ContentType = null;
-                c.WriteAsyncObject(f(e, c, b));
+                c.WriteAsyncObject(f((TException)e, c, b));
                 return Task.CompletedTask;
             }
 
