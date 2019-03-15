29ee19036da2aa28791c372aef85924292e21dfe
Joseph Woodward <joseph.woodward@xeuse.com>
2019-03-15 09:31:54 +0000

    4.0.2 release

--- package.sh
+++ package.sh
@@ -2,5 +2,5 @@ rm -rf ./package
 
 cd ./src/
 
-dotnet pack ./GlobalExceptionHandler/GlobalExceptionHandler.csproj -o ../../package/ -c release
-dotnet pack ./GlobalExceptionHandler.ContentNegotiation.Mvc/GlobalExceptionHandler.ContentNegotiation.Mvc.csproj -o ../../package/ -c release
+dotnet pack ./GlobalExceptionHandler/GlobalExceptionHandler.csproj -o ../package/ -c release
+dotnet pack ./GlobalExceptionHandler.ContentNegotiation.Mvc/GlobalExceptionHandler.ContentNegotiation.Mvc.csproj -o ../package/ -c release
