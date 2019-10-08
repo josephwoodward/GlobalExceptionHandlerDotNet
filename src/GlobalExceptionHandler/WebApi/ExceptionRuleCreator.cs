ddc383f65e8932f9332d280db264626519d2d2c2
Joseph Woodward <joseph.woodward@just-eat.com>
2019-10-08 09:02:40 +0100

    More removal|

--- src/GlobalExceptionHandler/WebApi/ExceptionRuleCreator.cs
+++ src/GlobalExceptionHandler/WebApi/ExceptionRuleCreator.cs
@@ -6,93 +6,12 @@ using Microsoft.AspNetCore.Http;
 
 namespace GlobalExceptionHandler.WebApi
 {
-    public interface IHasStatusCode
-    {
-        [Obsolete("ReturnStatusCode(..) is obsolete and will be removed soon, use ToStatusCode(..) instead", false)]
-        IHandledFormatters ReturnStatusCode(int statusCode);
-    }
-
     public interface IHasStatusCode<out TException> where TException: Exception
     {
         IHandledFormatters<TException> ToStatusCode(int statusCode);
         IHandledFormatters<TException> ToStatusCode(HttpStatusCode statusCode);
         IHandledFormatters<TException> ToStatusCode(Func<TException, int> statusCodeResolver);
         IHandledFormatters<TException> ToStatusCode(Func<TException, HttpStatusCode> statusCodeResolver);
-    }
-
-    internal class ExceptionRuleCreator : IHasStatusCode, IHandledFormatters
-    {
-        private readonly IDictionary<Type, ExceptionConfig> _configurations;
-        private readonly Type _currentFluentlyConfiguredType;
-
-        public ExceptionRuleCreator(IDictionary<Type, ExceptionConfig> configurations, Type currentFluentlyConfiguredType)
-        {
-            _configurations = configurations;
-            _currentFluentlyConfiguredType = currentFluentlyConfiguredType;
-        }
-
-        public IHandledFormatters ReturnStatusCode(int statusCode)
-            => ToStatusCode(statusCode);
-
-         IHandledFormatters ToStatusCode(int statusCode)
-            => ToStatusCode(ex => statusCode);
-
-         IHandledFormatters ToStatusCode(Func<Exception, int> statusCodeResolver)
-        {
-            var exceptionConfig = new ExceptionConfig
-            {
-                StatusCodeResolver = statusCodeResolver
-            };
-
-            _configurations.Add(_currentFluentlyConfiguredType, exceptionConfig);
-
-            return this;
-        }
-
-        public void UsingMessageFormatter(Func<Exception, HttpContext, string> formatter)
-            => WithBody(formatter);
-
-        public void UsingMessageFormatter(Func<Exception, HttpContext, Task> formatter)
-            => WithBody(formatter);
-
-        public void UsingMessageFormatter(Func<Exception, HttpContext, HandlerContext, Task> formatter)
-            => WithBody(formatter);
-
-        private void WithBody(Func<Exception, HttpContext, string> formatter)
-        {
-            Task Formatter(Exception x, HttpContext y, HandlerContext b)
-            {
-                var s = formatter.Invoke(x, y);
-                return y.Response.WriteAsync(s);
-            }
-
-            UsingMessageFormatter(Formatter);
-        }
-
-        private void WithBody(Func<Exception, HttpContext, Task> formatter)
-        {
-            if (formatter == null)
-                throw new NullReferenceException(nameof(formatter));
-
-            Task Formatter(Exception x, HttpContext y, HandlerContext b)
-            {
-                return formatter.Invoke(x, y);
-            }
-
-            UsingMessageFormatter(Formatter);
-        }
-
-        private void WithBody(Func<Exception, HttpContext, HandlerContext, Task> formatter)
-            => SetMessageFormatter(formatter);
-
-        private void SetMessageFormatter(Func<Exception, HttpContext, HandlerContext, Task> formatter)
-        {
-            if (formatter == null)
-                throw new NullReferenceException(nameof(formatter));
-
-	        var exceptionConfig = _configurations[_currentFluentlyConfiguredType];
-	        exceptionConfig.Formatter = formatter;
-        }
     }
 
     internal class ExceptionRuleCreator<TException> : IHasStatusCode<TException>, IHandledFormatters<TException> where TException: Exception
@@ -118,10 +37,10 @@ namespace GlobalExceptionHandler.WebApi
 
         private IHandledFormatters<TException> ToStatusCodeImpl(Func<TException, int> statusCodeResolver)
         {
-            int wrappedResolver(Exception x) => statusCodeResolver((TException)x);
+            int WrappedResolver(Exception x) => statusCodeResolver((TException)x);
             var exceptionConfig = new ExceptionConfig
             {
-                StatusCodeResolver = wrappedResolver
+                StatusCodeResolver = WrappedResolver
             };
 
             _configurations.Add(typeof(TException), exceptionConfig);
@@ -145,10 +64,8 @@ namespace GlobalExceptionHandler.WebApi
             if (formatter == null)
                 throw new NullReferenceException(nameof(formatter));
 
-            Task Formatter(TException x, HttpContext y, HandlerContext b)
-            {
-                return formatter.Invoke(x, y);
-            }
+            Task Formatter(TException x, HttpContext y, HandlerContext b) 
+                => formatter.Invoke(x, y);
 
             UsingMessageFormatter(Formatter);
         }
@@ -165,9 +82,9 @@ namespace GlobalExceptionHandler.WebApi
             if (formatter == null)
                 throw new NullReferenceException(nameof(formatter));
 
-            Task wrappedFormatter(Exception x, HttpContext y, HandlerContext z) => formatter((TException)x, y, z);
+            Task WrappedFormatter(Exception x, HttpContext y, HandlerContext z) => formatter((TException)x, y, z);
 	        var exceptionConfig = _configurations[typeof(TException)];
-	        exceptionConfig.Formatter = wrappedFormatter;
+	        exceptionConfig.Formatter = WrappedFormatter;
         }
     }
 }
