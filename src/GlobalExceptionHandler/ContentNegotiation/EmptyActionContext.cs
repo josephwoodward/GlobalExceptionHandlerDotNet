34b6e6ad789d230763cfa3b6779cd5bb2156c1f6
Joseph Woodward <joseph.woodward@just-eat.com>
2019-10-30 00:46:21 +0000

    Remove content negotiation project

--- src/GlobalExceptionHandler/ContentNegotiation/EmptyActionContext.cs
+++ src/GlobalExceptionHandler/ContentNegotiation/EmptyActionContext.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Routing;
