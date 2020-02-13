2b619808e552e6a420db9504a9b6401454eedcee
Joseph Woodward <joseph.woodward@just-eat.com>
2020-02-13 04:11:48 +0000

    Update package

--- package.sh
+++ package.sh
@@ -1,6 +1,5 @@
+#!/usr/bin/env bash
+
 rm -rf ./package
 
-cd ./src/
-
-dotnet pack ./GlobalExceptionHandler/GlobalExceptionHandler.csproj -o ../package/ -c release
-dotnet pack ./GlobalExceptionHandler.ContentNegotiation.Mvc/GlobalExceptionHandler.ContentNegotiation.Mvc.csproj -o ../package/ -c release
+dotnet pack ./src/GlobalExceptionHandler/GlobalExceptionHandler.csproj -o ./package/ -c release
